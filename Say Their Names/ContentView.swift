83ecd447a304c89a13609f0d7530e8ea0728201f
Ian <ian@fruitstreet.com>
2020-06-28 18:57:39 -0700

    Initial Commit

---
+++ Say Their Names/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Say Their Names
+//
+//  Created by Ian McFarland on 6/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
