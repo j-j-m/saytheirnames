8cc82f79900fd48c601961721b9c3230cdf4036e
Ian <ian@fruitstreet.com>
2020-06-28 23:08:04 -0700

    Starting pagination.

--- Say Their Names/ContentView.swift
+++ Say Their Names/ContentView.swift
@@ -11,33 +11,36 @@ import AVFoundation
 struct ContentView: View {
     let shootingIncidents = Bundle.main.decode([IncidentRecord].self, from: "shooting-incidents")
 
-
-
     var body: some View {
         ScrollView {
             VStack {
-                ForEach(shootingIncidents, id: \.id) { incident in
-//                    HStack {
+                ForEach(getPaginatedList(offset: 0), id: \.id) { incident in
+                    HStack {
 //                        VStack {
                             Text(incident.victimName).bold()
 //                            Text("Killed by \(incident.responsibleAgency) in \(incident.city), \(incident.state)")
 //                        }
-//                        if (incident.victimName != "Name withheld by police") {
-//                            Button("Say name", action: {
-//                                let utterance = AVSpeechUtterance(string: incident.victimName)
-//                                utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-//                                utterance.rate = 0.1
-//
-//                                let synthesizer = AVSpeechSynthesizer()
-//                                synthesizer.speak(utterance)
-//                            })
-//                        }
+                        if (incident.victimName != "Name withheld by police") {
+                            Button("Say name", action: {
+                                let utterance = AVSpeechUtterance(string: incident.victimName)
+                                utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                                utterance.rate = 0.1
+
+                                let synthesizer = AVSpeechSynthesizer()
+                                synthesizer.speak(utterance)
+                            })
+                        }
                     }
                 }
-//            }
+            }
         }
     }
+    
+    func getPaginatedList(offset: Int) -> ArraySlice<IncidentRecord> {
+        return self.shootingIncidents[offset..<(offset+100)]
+    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
