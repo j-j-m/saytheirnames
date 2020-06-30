b1f79e6e59f58b7203293216f4f6ef4a352f850f
Ian <ian@fruitstreet.com>
2020-06-30 14:11:20 -0700

    Extracted button behavior into methods, and made a "say their names" button.

--- Say Their Names/ContentView.swift
+++ Say Their Names/ContentView.swift
@@ -10,22 +10,22 @@ import AVFoundation
 
 struct ContentView: View {
     let shootingIncidents = Bundle.main.decode([IncidentRecord].self, from: "shooting-incidents")
+    let nameSayer = NameSayer()
 
     var body: some View {
         ScrollView {
             VStack {
+                Button("Say their names!", action: {
+                    saySomeNames(incidents: shootingIncidents, howMany: 20)
+                })
+                Spacer()
                 ForEach(getPaginatedList(offset: 0), id: \.id) { incident in
                     VStack {
                         HStack {
                             Text(incident.victimName).bold()
                             if (incident.victimName != "Name withheld by police") {
                                 Button("Say \(incident.victimGender == "Male" ? "his" : incident.victimGender == "Female" ? "her" : "their") name", action: {
-                                    let utterance = AVSpeechUtterance(string: incident.victimName)
-                                    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
-                                    utterance.rate = 0.5
-
-                                    let synthesizer = AVSpeechSynthesizer()
-                                    synthesizer.speak(utterance)
+                                    say(whatToSay: incident.victimName)
                                 })
                             }
                         }
@@ -34,6 +34,34 @@ struct ContentView: View {
                 }
             }
         }
+    }
+    
+    func say(whatToSay: String) {
+        let utterance = AVSpeechUtterance(string: whatToSay)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.rate = 0.4
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
+    
+    func saySomeNames(incidents: [IncidentRecord], howMany: Int) {
+        let range  = incidents.count - howMany
+        let offset = Int.random(in: 0 ..< range)
+        let limit = offset + howMany
+        
+        sayTheirNames(incidents: incidents[offset ..< limit])
+
+    }
+    
+    func sayTheirNames(incidents: ArraySlice<IncidentRecord>) {
+        var stuffToSay = ""
+        incidents.forEach { incident in
+            if (incident.victimName != "Name withheld by police") {
+                stuffToSay = "\(stuffToSay) Say \(incident.victimGender == "Male" ? "his" : incident.victimGender == "Female" ? "her" : "their") name! \(incident.victimName)!"
+            }
+        }
+        say(whatToSay: stuffToSay)
     }
     
     func getPaginatedList(offset: Int) -> ArraySlice<IncidentRecord> {
